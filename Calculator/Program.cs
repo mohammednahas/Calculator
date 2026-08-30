27c47e45d9274f9ceda6d6e784b6fe943f3cc525
mohammed elnahas <m.moustafa.elnahas@gmail.com>
2026-08-30 23:07:16 +0300

    ADD calculator-menu

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("====================");
+Console.WriteLine("     CALCULATOR");
+Console.WriteLine("====================");
+
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Subtraction");
+Console.WriteLine("3. Multiplication");
+Console.WriteLine("4. Division");
+Console.WriteLine("5. Exit");
+
+Console.Write("Choose an option: ");
+
+string? choice = Console.ReadLine();
+
+Console.WriteLine($"You selected: {choice}");
